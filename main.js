35d8e14cfa40eaaf15efe05755281e03c91d558f
Nelson Camargo <cmargosb@gmail.com>
2023-04-14 02:02:14 +0200

    ultimo intento

--- main.js
+++ main.js
@@ -1,23 +1,45 @@
-import {
-  cloud1,
-  cloud2,
-  moon,
-  nombre,
-  line,
-  line2,
-  developer,
-  btnMoon,
-  btnSun,
-  btnSwitch,
-  btnmenu,
-  switchAfter,
-  menuBtn,
-  contactBTN,
-  mainBtn,
-  tecnology,
-  about_left,
-  about_rigth
-} from "/variables.js";
+// import {
+//   cloud1,
+//   cloud2,
+//   moon,
+//   nombre,
+//   line,
+//   line2,
+//   developer,
+//   btnMoon,
+//   btnSun,
+//   btnSwitch,
+//   btnmenu,
+//   switchAfter,
+//   menuBtn,
+//   contactBTN,
+//   mainBtn,
+//   tecnology,
+//   about_left,
+//   about_rigth,
+// } from "/variables.js";
+
+
+let cloud2 = document.querySelector(".nube2");
+let cloud1 = document.querySelector(".nube1");
+let moon = document.querySelector(".moon");
+let nombre = document.querySelector("h1");
+let line = document.querySelector(".l1");
+let line2 = document.querySelector(".l2");
+let developer = document.querySelector(".developer");
+const btnSwitch = document.querySelector(".switch");
+const switchAfter = document.querySelector(".switch-after");
+const btnSun = document.querySelector(".fa-sun");
+const btnMoon = document.querySelector(".fa-moon");
+const menuBtn = document.querySelector(".menu-btn");
+const btnmenu = document.querySelector(".btnmenu");
+const contactBTN = document.getElementById("cont-btn");
+const mainBtn = document.getElementById("main-btn");
+const imgMoon = document.querySelector(".moon");
+const about_left = document.querySelector(".des1");
+const about_rigth = document.querySelector(".des2");
+const tecnology = document.querySelector(".tecnology");
+const card = document.querySelector('.card');
 
 menuBtn.addEventListener("click", () => {
   btnmenu.classList.toggle("btnmenu");
